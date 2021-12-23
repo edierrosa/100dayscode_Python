257a530c7fd6518b9b9868acfb8f161ea794edf3
edierrosa <edierrosa@gmail.com>
2021-12-23 11:52:57 -0300

    course files day 6

---
+++ day_6_project-escaping_the_maze.py
@@ -0,0 +1,18 @@
+# Based on Reeborg's World
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if right_is_clear():
+        if front_is_clear():
+            move()
+        else:
+            turn_right()
+            move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
