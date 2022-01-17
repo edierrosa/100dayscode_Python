053bbae6f460357e59989face7398c010f856870
edierrosa <edierrosa@gmail.com>
2022-01-16 22:20:07 -0300

    course files day 10

---
+++ day_10_project-calculator.py
@@ -0,0 +1,71 @@
+# Import art and os
+import calculator_art
+import os
+
+# Defines addition function
+
+
+def add(n1, n2):
+    return n1 + n2
+
+# Defines subtration function
+
+
+def sub(n1, n2):
+    return n1 - n2
+
+# Defines multiplication function
+
+
+def mult(n1, n2):
+    return n1 * n2
+
+# Defines division function
+
+
+def div(n1, n2):
+    return n1 / n2
+
+
+# Creates a operations dictionary
+operations = {
+    "+": add,
+    "-": sub,
+    "*": mult,
+    "/": div,
+}
+
+# Defines calculation function
+
+
+def calculate(num1=None):
+    # Asks for input
+    if num1 == None:
+        num1 = float(input("What's the first number? "))
+    op = input(
+        f"Choose an operation ({', '.join(key for key in operations)}): ")
+    num2 = float(input("What's the next number? "))
+    # Performs the calculation
+    answer = operations[op](num1, num2)
+    # Prints result
+    print(f"{num1} {op} {num2} = {answer}")
+    return answer
+
+
+# Run the calculator
+running = True
+number = None
+while running:
+    if number == None:
+        print(calculator_art.logo)
+        number = calculate()
+    else:
+        run = input(
+            f"Type 'y' to continue calculating with {number}, 'n' to start a new calculation, or 'e' to exit: ")
+        if run == "y":
+            number = calculate(num1=number)
+        elif run == "n":
+            number = None
+            os.system('cls' if os.name == 'nt' else 'clear')
+        else:
+            running = False
